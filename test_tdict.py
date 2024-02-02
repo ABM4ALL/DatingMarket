1da32ba5054e9e65f90d9c573a7b4ee3531a52af
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2024-02-03 07:25:49 +0800

    test_tdict comment added

--- test_tdict.py
+++ test_tdict.py
@@ -6,6 +6,26 @@ from tab2dict import TabDict
 from utils.tabkey import DatingTabKey
 
 
+"""
+This script shows how to use `tab2dict` package for loading data into a model.
+
+First, `tab2dict` can automatically identifies
+ - `tdict_type`: according to the prefix of file names: ID_, Relation_, Data_
+ - `key_cols`: with `id_` or `time_` prefix in the column names
+in an `.xlsx` or `.csv` file, and converts the input file into a `tdict`.
+The two functions below show how to load an input table with `tab2dict`.
+ 
+Second, `tdict` works together with a `tkey`:
+ - a `tkey` is an instance of a class inheriting the `TabKey` class provided by the `tab2dict` package
+ - with a `tkey` that knows the values of the `key_cols` of a `tdict`, one can fetch data from the `tdict`
+
+In ABMs, each agent is assigned with a `tkey`, like an "identity" of the agent. 
+With this `tkey`, the agent can get data from the `tdict`s. The advantage is, 
+the agents usually know more than necessary in their `tkey`s for fetching data from individual `tdict`s.
+So, users may be able to flexibly add new `id_` or `time_` columns in the input tables without changing code.
+"""
+
+
 def load_tdict_file(file_name: str, col: str = "value"):
     file_path = os.path.join("data", "input", file_name)
     return TabDict.from_file(file_path=file_path, value_column_name=col)
