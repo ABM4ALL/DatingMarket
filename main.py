afaf841ed94e67e8e77ac96d139ad0954f1c087e
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2024-02-06 05:59:02 +0800

    comments added

--- main.py
+++ main.py
@@ -9,20 +9,23 @@ from core.scenario import DatingScenario
 
 def run_model(config):
     """
-    By calling the `simulator.run()` function, we go through the following steps:
+    By calling `simulator.run()`, we ask Melodie to run the model for all the scenarios,
+    which are defined as rows in the `SimulatorScenario.xlsx` file.
+    For each scenario, Melodie will run the model `run_num` times.
+
+    More specifically, we go through the following steps:
 
     First, based on the rows in the `SimulatorScenario.xlsx` file, Melodie will create a list of `scenario` objects.
     At the same time, each `scenario` object also calls its `setup` and `load_data` functions.
 
-    Second, for each `scenario`, Melodie will run the model.
-    So, a model object will be created with each `scenario` object, as well as other components,
+    Second, when running one `scenario`, a model object will be created, as well as its components,
     including agent_list(s), environment, data_collector.
-    Two functions of the `model` object are called in this step:
+    Two functions of the `model` object are called:
      - model.create() --> the model components are created, including agent_list(s), environment, data_collector.
      For each component, its `setup` function is also called to create the parameter/variable attributes.
      - model.setup() --> the model components are filled in with data, especially the agent_list(s).
 
-    Third, after instantiating and setting up the scenario and model (incl. the components),
+    Third, after setting up the `model` and its components for this `scenario`,
     the `simulator` will call `model.run()` to run the model under the current `scenario` and save the results.
     After finishing, the `simulator` automatically starts running the next scenario.
     """
