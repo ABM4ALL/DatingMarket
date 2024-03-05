5ac3c0a854fd0ae7695d6dbcac2997cf243db598
Songmin Yu <songmin.yu@isi.fraunhofer.de>
2024-03-05 15:10:08 +0100

    save_dataframe moved to Melodie

--- core/data_collector.py
+++ core/data_collector.py
@@ -25,16 +25,3 @@ class DatingDataCollector(DataCollector):
         self.add_agent_property(container_name="women", property_name="social")
         self.add_agent_property(container_name="women", property_name="saving")
         self.add_environment_property(property_name="total_assessment")
-
-    def save_dataframe(self, df: pd.DataFrame, df_name: str, if_exists: str = 'append'):
-        path = os.path.join(self.config.output_folder, f'{df_name}.csv')
-        if os.path.isfile(path):
-            if if_exists == "append":
-                df.to_csv(path, mode='a', header=False, index=False)
-            elif if_exists == "replace":
-                df.to_csv(path, index=False)
-            else:
-                raise NotImplementedError(f'if_exists = {if_exists} --> not implemented.')
-        else:
-            df.to_csv(path, index=False)
-
